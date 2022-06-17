f9709536534763ab3b54cd92203bfe80436dbed4
Luanderson Mendes <luandersonpmendes@hotmail.com>
2022-06-17 20:16:51 -0300

    project

---
+++ src/assets/js/index.js
@@ -0,0 +1,22 @@
+
+
+const fibo = document.querySelector('.fibonacci')
+const dados = document.querySelector('.dados')
+const percentual = document.querySelector('.percentual')
+const string = document.querySelector('.string')
+
+fibo.addEventListener('click', function(e) {
+    window.location.assign('../../https/Fibonacci/fibonacci.html') 
+})
+
+dados.addEventListener('click', function(e) {
+    window.location.assign('../../https/Fibonacci/fibonacci.html') 
+})
+
+percentual.addEventListener('click', function(e) {
+    window.location.assign('../../https/Fibonacci/fibonacci.html') 
+})
+
+string.addEventListener('click', function(e) {
+    window.location.assign('../../https/Fobonacci/fibonacci.html') 
+})
--- src/assets/js/inversorDeString.js
+++ src/assets/js/inversorDeString.js
@@ -1,10 +1,57 @@
+const form = document.querySelector("#formulario")
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    const inputString = e.target.querySelector('#string')
+
+    const text = inputString.value
+
+    if (!text){
+        setResultado('Campo não pode estar vázio', false)
+        return
+    }
+
+    const msg = reverseString(text)
+    
+    setResultado(msg, true)
+})
+
+
+function criaP () {
+    const p = document.createElement('p')
+    return p
+}
+function criaH4() {
+    const h4 = document.createElement('h4')
+    return h4
+}
+
+
+function setResultado (msg, isValid) {
+    const resultado = document.querySelector('#resultado')
+    resultado.innerHTML = ''
+
+    const p = criaP()
+    const h4 = criaH4()
+
+    if (isValid) {
+        h4.innerHTML = "Texto Invertido"
+        resultado.appendChild(h4)
+        p.classList.add('paragrafo-resultado')
+    } else {
+        p.classList.add('bad')
+    }
+
+    p.innerHTML = msg;
+    resultado.appendChild(p)
+}
+
 function reverseString(text) {
     let normalString = [...text]
     let reversedString = []
     for (let i = normalString.length; i > 0; i--){
         reversedString.push(normalString[i-1])
     }
-    console.log(reversedString.join(''))  
+    return reversedString.join('')
 }
 
-reverseString('Luanderson Mendes')
