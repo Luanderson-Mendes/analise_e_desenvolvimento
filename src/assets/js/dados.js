4a821c110e676f8e2e7ea2a76bad6e28bcda3aa7
Luanderson Mendes <luandersonpmendes@hotmail.com>
2022-06-18 20:29:51 -0300

    commit final

--- src/assets/js/dados.js
+++ src/assets/js/dados.js
@@ -1,54 +1,92 @@
-const form = document.querySelector('#formulario')
-
-form.addEventListener('submit', function(e){
-    e.preventDefault()
-})
-
 const dados = fetch('../../assets/data/dados.json')
 .then(function(res) {
     return res.json()
 }).then(function(dados){
-    const newData = dados.filter((item) => item.valor !== 0)
-    const total = newData.map((item) => item.valor).reduce((prev, curr) => prev + curr, 0)
-    const mediaMensal = total / newData.length
-    
-    const maiorValor = newData.map((item) => item.valor).reduce((ini, end) => Math.max(ini, end))
-    const menorValor = newData.map((item) => item.valor).reduce((ini, end) => Math.min(ini, end))
-    const faturamentoMaiorMedia = newData.filter((item) => item.valor > mediaMensal).length
+    const form = document.querySelector('#formulario')
 
-    let col = []
+    form.addEventListener('submit', function(e){
+        e.preventDefault()
 
-    for (let i = 0; i < newData.length; i++){
-        for (let key in newData[i]){
-            if(col.indexOf(key) === -1){
-                col.push(key)
+        let divTable = document.querySelector('.table')
+        let newData = dados.filter((item) => item.valor !== 0)
+        const total = newData.map((item) => item.valor).reduce((prev, curr) => prev + curr, 0)
+        const mediaMensal = total / newData.length
+            
+        const maiorValor = newData.map((item) => item.valor).reduce((ini, end) => Math.max(ini, end))
+        const menorValor = newData.map((item) => item.valor).reduce((ini, end) => Math.min(ini, end))
+
+        const faturamentoMaiorMedia = newData.filter((item) => item.valor > mediaMensal)
+        const qntfaturamentoMaiorMedia = newData.filter((item) => item.valor > mediaMensal).length
+        const valorMaior = newData.filter((item) => item.valor === maiorValor)
+        const valorMenor = newData.filter((item) => item.valor === menorValor)
+ 
+
+        for(let i = 0; i < form.length; i++){
+            if(form[i].checked){
+                switch (form[i].value) {
+                    case 'total-table':
+                        tabela(newData)
+                        break;
+                    case 'max-value':
+                        tabela(valorMaior)
+                        break;
+                    case 'min-value':
+                        tabela(valorMenor)
+                        break;
+                    case 'higher-media':
+                        tabela(faturamentoMaiorMedia)
+                        let h4 = document.createElement('h4')
+                        h4.innerHTML = `
+                            Total de dias: ${qntfaturamentoMaiorMedia} </br>
+                            Média Mensal: ${mediaMensal.toFixed(4)}
+                        `
+                        divTable.appendChild(h4)
+                        break;
+                    default:
+                        break;
+                }
             }
         }
-    }
 
-    let table = document.createElement('table')
-
-    let tr = table.insertRow()
-
-    for(let i = 0; i < col.length; i++){
-        let th = document.createElement('th')
-        th.innerHTML = col[i]
-        tr.appendChild(th)
-    }
-
-    for (let i = 0; i < newData.length; i++){
-        tr = table.insertRow()
-        let tabDia = document.createElement('td')
-        let tabValor = document.createElement('td')
-        let dia = newData[i].dia, valor = newData[i].valor
-        tabDia.innerHTML = dia
-        tabValor.innerHTML = valor
-        tr.appendChild(tabDia)
-        tr.appendChild(tabValor)      
         
-    }
 
-    let divTable = document.querySelector('.table')
-    divTable.innerHTML = ''
-    divTable.appendChild(table)
+        function tabela(dados){
+            let col = []
+    
+            for (let i = 0; i < dados.length; i++){
+                for (let key in dados[i]){
+                    if(col.indexOf(key) === -1){
+                        col.push(key)
+                    }
+                }
+            }
+    
+            let table = document.createElement('table')
+    
+            let tr = table.insertRow()
+    
+            for(let i = 0; i < col.length; i++){
+                let th = document.createElement('th')
+                th.innerHTML = col[i]
+                tr.appendChild(th)
+            }
+    
+            for (let i = 0; i < dados.length; i++){
+                tr = table.insertRow()
+                let tabDia = document.createElement('td')
+                let tabValor = document.createElement('td')
+                let dia = dados[i].dia, valor = dados[i].valor
+                tabDia.innerHTML = dia
+                tabValor.innerHTML = valor
+                tr.appendChild(tabDia)
+                tr.appendChild(tabValor)      
+                
+            }
+    
+            let divTable = document.querySelector('.table')
+            divTable.innerHTML = ''
+            divTable.appendChild(table)
+        }
+    })
+
 })
--- src/assets/js/percentualDeRepresentacao.js
+++ src/assets/js/percentualDeRepresentacao.js
@@ -8,10 +8,52 @@ function calculaPercentual(){
         {estado: 'outros', valor: 19849.53},]
 
     const total = faturamento.map(item => item.valor).reduce((prev, curr) => prev + curr, 0)
+    faturamento.push({estado: 'total', valor: total})
+    // console.log(faturamento)
     const percentual = faturamento.map(e => e.valor / total * 100)
     for (let i = 0; i < percentual.length; i++){
-        console.log(parseInt(percentual[i]).toFixed())
-    } 
+        faturamento[i]['percentual'] = percentual[i].toFixed()
+    }
+    console.log(faturamento)
+
+    let col = []
+
+    for (let i = 0; i < faturamento.length; i++){
+        for (let key in faturamento[i]){
+            if(col.indexOf(key) === -1){
+                col.push(key)
+            }
+        }
+    }
+
+    let table = document.createElement('table')
+
+    let tr = table.insertRow()
+
+    for(let i = 0; i < col.length; i++){
+        let th = document.createElement('th')
+        th.innerHTML = col[i]
+        tr.appendChild(th)
+    }
+
+    for (let i = 0; i < faturamento.length; i++){
+        tr = table.insertRow()
+        let tabDia = document.createElement('td')
+        let tabValor = document.createElement('td')
+        let tabPercent = document.createElement('td')
+        let dia = faturamento[i].estado, valor = faturamento[i].valor, percent = faturamento[i].percentual
+        tabDia.innerHTML = dia
+        tabValor.innerHTML = valor
+        tabPercent.innerHTML = `${percent}%`
+        tr.appendChild(tabDia)
+        tr.appendChild(tabValor)      
+        tr.appendChild(tabPercent)
+        
+    }
+
+    let divTable = document.querySelector('.table')
+    divTable.innerHTML = ''
+    divTable.appendChild(table)
 }
 
 calculaPercentual()
